0bd928290e701b1eb12740e385f862fd3b683f1d
david <mrminecraft00@gmail.com>
2024-06-09 16:39:01 +0200

    updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,21 +17,20 @@ repositories {
 }
 
 dependencies {
-    compileOnly("org.jetbrains:annotations:24.0.0")
-    compileOnly("org.projectlombok:lombok:1.18.30")
+    compileOnly("org.jetbrains:annotations:24.1.0")
+    compileOnly("org.projectlombok:lombok:1.18.32")
 
-    implementation("org.xerial:sqlite-jdbc:3.7.2")
+    implementation("org.xerial:sqlite-jdbc:3.41.2.2")
     implementation("com.sparkjava:spark-core:2.9.4")
-    implementation("com.google.code.gson:gson:2.10.1")
-    implementation("ch.qos.logback:logback-classic:1.2.6")
-
+    implementation("com.google.code.gson:gson:2.11.0")
+    implementation("ch.qos.logback:logback-classic:1.5.6")
 
     implementation("net.thenextlvl.core:annotations:2.0.1")
-    implementation("net.thenextlvl.core:files:1.0.3")
-    implementation("net.thenextlvl.core:utils:1.0.8")
-    implementation("net.thenextlvl.core:nbt:1.3.9")
+    implementation("net.thenextlvl.core:files:1.0.4")
+    implementation("net.thenextlvl.core:utils:1.0.9")
+    implementation("net.thenextlvl.core:nbt:1.4.1")
 
-    annotationProcessor("org.projectlombok:lombok:1.18.30")
+    annotationProcessor("org.projectlombok:lombok:1.18.32")
 }
 
 tasks.jar {
