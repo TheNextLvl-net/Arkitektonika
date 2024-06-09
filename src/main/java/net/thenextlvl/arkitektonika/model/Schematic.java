8cc7a08ae14ad286df757f9472d69744498cdc7e
david <mrminecraft00@gmail.com>
2024-06-09 17:08:19 +0200

    make schematic and its values non-final

--- src/main/java/net/thenextlvl/arkitektonika/model/Schematic.java
+++ src/main/java/net/thenextlvl/arkitektonika/model/Schematic.java
@@ -1,14 +1,27 @@
 package net.thenextlvl.arkitektonika.model;
 
+import lombok.*;
+import lombok.experimental.Accessors;
+import net.thenextlvl.arkitektonika.Arkitektonika;
+
 import java.sql.Date;
 
-public record Schematic(
-        String downloadKey,
-        String deleteKey,
-        String fileName,
-        Date lastAccessed
-) {
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Accessors(fluent = true)
+public class Schematic {
+    private final String downloadKey;
+    private final String deleteKey;
+    private String fileName;
+    private Date expirationDate;
+
     public Schematic(String downloadKey, String deleteKey, String fileName) {
-        this(downloadKey, deleteKey, fileName, new Date(System.currentTimeMillis()));
+        this(downloadKey, deleteKey, fileName, new Date(
+                System.currentTimeMillis() + Arkitektonika.CONFIG.prune()
+        ));
     }
 }
