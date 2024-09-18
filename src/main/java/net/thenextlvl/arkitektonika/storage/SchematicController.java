fadc3d756f9f4057b469776720bf8a1c8e52c92b
david <mrminecraft00@gmail.com>
2024-09-18 20:45:08 +0200

    Add SchematicController for asynchronous schematic management
    Introduced a new class, SchematicController, to handle asynchronous operations related to schematics in the Arkitektonika system. This controller facilitates deletion, pruning, and retrieval of schematics using various keys through CompletableFutures. This enhancement improves the modularity and scalability of schematic-related data operations.

---
+++ src/main/java/net/thenextlvl/arkitektonika/storage/SchematicController.java
@@ -0,0 +1,33 @@
+package net.thenextlvl.arkitektonika.storage;
+
+import lombok.RequiredArgsConstructor;
+import net.thenextlvl.arkitektonika.Arkitektonika;
+import net.thenextlvl.arkitektonika.model.Schematic;
+
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+
+@RequiredArgsConstructor
+public class SchematicController {
+    private final Arkitektonika arkitektonika;
+
+    public CompletableFuture<Boolean> delete(String deletionKey) {
+        return CompletableFuture.supplyAsync(() -> arkitektonika.dataController().removeSchematic(deletionKey));
+    }
+
+    public CompletableFuture<Integer> prune() {
+        return CompletableFuture.supplyAsync(() -> arkitektonika.dataController().pruneSchematics());
+    }
+
+    public CompletableFuture<Optional<Schematic>> getByDeletionKey(String key) {
+        return CompletableFuture.supplyAsync(() -> arkitektonika.dataController().getSchematicByDeletionKey(key));
+    }
+
+    public CompletableFuture<Optional<Schematic>> getByDownloadKey(String key) {
+        return CompletableFuture.supplyAsync(() -> arkitektonika.dataController().getSchematicByDownloadKey(key));
+    }
+
+    public CompletableFuture<Optional<Schematic>> getByKey(String key) {
+        return CompletableFuture.supplyAsync(() -> arkitektonika.dataController().getSchematicByKey(key));
+    }
+}
