9ba2d66de183ceac8e2de3141139d83fd9340375
david <mrminecraft00@gmail.com>
2024-09-18 20:43:47 +0200

    Replace Spark with Javalin for RenameRoute
    The RenameRouter class using Spark was deleted and replaced with the RenameRoute class using Javalin. This migration improves route handling and unifies it under the Javalin framework, which simplifies the server configuration and management.

---
+++ src/main/java/net/thenextlvl/arkitektonika/routes/RenameRoute.java
@@ -0,0 +1,35 @@
+package net.thenextlvl.arkitektonika.routes;
+
+import io.javalin.http.Context;
+import lombok.RequiredArgsConstructor;
+import net.thenextlvl.arkitektonika.Arkitektonika;
+
+@RequiredArgsConstructor
+public class RenameRoute {
+    private final Arkitektonika arkitektonika;
+
+    public void register() {
+        arkitektonika.javalin().put("/rename/{key}/{name}", this::rename);
+        arkitektonika.javalin().options("/rename/{key}/{name}", context -> {
+            context.header("Access-Control-Allow-Methods", "PUT");
+            context.status(204);
+        });
+    }
+
+    private void rename(Context context) {
+        arkitektonika.schematicController()
+                .getByDeletionKey(context.pathParam("key"))
+                .thenAccept(optional -> optional.ifPresentOrElse(schematic -> {
+                    arkitektonika.dataController().renameSchematic(schematic);
+                    context.result("File was renamed");
+                    context.status(200);
+                }, () -> {
+                    context.result("File not found");
+                    context.status(404);
+                })).exceptionally(throwable -> {
+                    context.result(throwable.getMessage());
+                    context.status(500);
+                    return null;
+                });
+    }
+}
