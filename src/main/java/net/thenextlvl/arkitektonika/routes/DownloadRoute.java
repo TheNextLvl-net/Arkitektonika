03210b69690296d383e4716a96b98caf30651114
david <mrminecraft00@gmail.com>
2024-09-18 20:43:39 +0200

    Migrate download routes from Spark to Javalin
    Replaced the `DownloadRouter` class using Spark with `DownloadRoute` class using Javalin. This transition involves updating route definitions and handling methods to align with Javalin's API, ensuring smoother request handling and more concise code.

---
+++ src/main/java/net/thenextlvl/arkitektonika/routes/DownloadRoute.java
@@ -0,0 +1,35 @@
+package net.thenextlvl.arkitektonika.routes;
+
+import io.javalin.http.Context;
+import lombok.RequiredArgsConstructor;
+import net.thenextlvl.arkitektonika.Arkitektonika;
+
+@RequiredArgsConstructor
+public class DownloadRoute {
+    private final Arkitektonika arkitektonika;
+
+    public void register() {
+        arkitektonika.javalin().get("/download/{key}", this::download);
+        arkitektonika.javalin().options("/download/{key}", context -> {
+            context.header("Access-Control-Allow-Methods", "HEAD, GET");
+            context.status(204);
+        });
+    }
+
+    private void download(Context context) {
+        context.future(() -> arkitektonika.schematicController()
+                .getByDownloadKey(context.pathParam("key"))
+                .thenAccept(optional -> optional.ifPresentOrElse(schematic -> {
+                    context.header("Content-Disposition", "attachment; filename=" + schematic.name());
+                    context.result(schematic.data());
+                    context.status(200);
+                }, () -> {
+                    context.result("File not found");
+                    context.status(404);
+                })).exceptionally(throwable -> {
+                    context.result(throwable.getMessage());
+                    context.status(500);
+                    return null;
+                }));
+    }
+}
