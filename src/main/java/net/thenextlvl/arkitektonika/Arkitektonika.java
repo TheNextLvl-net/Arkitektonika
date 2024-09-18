6a476b0be2cfb052f7852fbe700eba05ecb4dca0
david <mrminecraft00@gmail.com>
2024-09-18 20:41:47 +0200

    Refactor Arkitektonika class to use Javalin and controllers
    Replaced Spark with Javalin for handling HTTP routes and added the use of controllers for data and schematic management. Simplified prune operation and access control setup, improving code organization and maintainability.

--- src/main/java/net/thenextlvl/arkitektonika/Arkitektonika.java
+++ src/main/java/net/thenextlvl/arkitektonika/Arkitektonika.java
@@ -2,100 +2,84 @@ package net.thenextlvl.arkitektonika;
 
 import core.file.format.GsonFile;
 import core.io.IO;
+import io.javalin.Javalin;
+import io.javalin.config.SizeUnit;
+import lombok.Getter;
+import lombok.SneakyThrows;
+import lombok.experimental.Accessors;
 import net.thenextlvl.arkitektonika.config.Config;
 import net.thenextlvl.arkitektonika.routes.*;
-import net.thenextlvl.arkitektonika.storage.DataStorage;
-import net.thenextlvl.arkitektonika.storage.DatabaseStorage;
+import net.thenextlvl.arkitektonika.storage.DataController;
+import net.thenextlvl.arkitektonika.storage.SQLiteController;
+import net.thenextlvl.arkitektonika.storage.SchematicController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import spark.Spark;
 
 import java.io.File;
-import java.sql.SQLException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
+@Getter
+@Accessors(fluent = true)
 public class Arkitektonika {
-    public static final File DATA_FOLDER = new File("data");
-    public static final File SCHEMATIC_FOLDER = new File(DATA_FOLDER, "schematics");
+    private static final Logger logger = LoggerFactory.getLogger(Arkitektonika.class);
 
-    public static final Config CONFIG = new GsonFile<>(IO.of(DATA_FOLDER, "config.json"), new Config(
+    private final File dataFolder = new File("data");
+    private final Config config = new GsonFile<>(IO.of(dataFolder, "config.json"), new Config(
             3000, TimeUnit.MINUTES.toMillis(30), 1000000, "*",
             new Config.Limiter(TimeUnit.SECONDS.toMillis(60), 30, 500)
     )).validate().save().getRoot();
 
-    public static final DataStorage DATA_STORAGE = new DatabaseStorage();
-    private static final Logger logger = LoggerFactory.getLogger(Arkitektonika.class);
+    private final DataController dataController;
+    private final SchematicController schematicController = new SchematicController(this);
 
-    static {
-        //noinspection ResultOfMethodCallIgnored
-        SCHEMATIC_FOLDER.mkdirs();
-    }
+    private final Javalin javalin = Javalin.create(config -> {
+        config.jetty.multipartConfig.maxFileSize(100, SizeUnit.MB);
+        config.jetty.multipartConfig.maxInMemoryFileSize(10, SizeUnit.MB);
+        config.jetty.multipartConfig.maxTotalRequestSize(1, SizeUnit.GB);
+        config.showJavalinBanner = false;
+    });
 
     public static void main(String[] args) {
-        try (var executorService = Executors.newSingleThreadScheduledExecutor()) {
-            executorService.scheduleAtFixedRate(Arkitektonika::prune, 0, CONFIG.prune(), TimeUnit.MILLISECONDS);
-            Spark.port(CONFIG.port());
-            registerAccessControl();
-            Base64Router.register();
-            DeleteRouter.register();
-            DownloadRouter.register();
-            ExpirationRouter.register();
-            RenameRouter.register();
-            UploadRouter.register();
+        new Arkitektonika().start();
+    }
+
+    @SneakyThrows
+    private Arkitektonika() {
+        this.dataController = new SQLiteController(dataFolder);
+        setupAccessControl();
+        registerRoutes();
+    }
+
+    private void start() {
+        try (var executor = Executors.newSingleThreadScheduledExecutor()) {
+            executor.scheduleAtFixedRate(this::prune, 0, config.prune(), TimeUnit.MILLISECONDS);
+            javalin.start(Integer.getInteger("port", config.port()));
         }
     }
 
-    private static void registerAccessControl() {
-        Spark.before((request, response) -> response.header("Access-Control-Allow-Origin", CONFIG.allowedOrigin()));
+    private void prune() {
+        var start = System.currentTimeMillis();
+        logger.info("Starting pruning expired schematics...");
+        schematicController.prune().thenAccept(amount -> {
+            var time = (System.currentTimeMillis() - start) / 1000d;
+            logger.info("Pruned {} schematics. Took {}s", amount, time);
+        }).exceptionally(throwable -> {
+            logger.error("Failed to prune schematics", throwable);
+            return null;
+        });
     }
 
-    public static void prune() {
-        try {
-            logger.info("Starting prune of old and unused schematics...");
-            int outdated = outdatedPrune();
-            int dangling = danglingPrune();
-            int invalid = invalidPrune();
-            logger.info("Pruned {} schematics ({} outdated, {} dangling, {} invalid)",
-                    outdated + dangling + invalid, outdated, dangling, invalid);
-        } catch (SQLException e) {
-            logger.error("Failed to perform a prune", e);
-        }
+    private void registerRoutes() {
+        new Base64Route(this).register();
+        new DeleteRoute(this).register();
+        new DownloadRoute(this).register();
+        new ExpirationRoute(this).register();
+        new RenameRoute(this).register();
+        new UploadRoute(this).register();
     }
 
-    private static int invalidPrune() throws SQLException {
-        var deletionCounter = 0;
-        for (var record : DATA_STORAGE.getSchematics()) {
-            if (new File(SCHEMATIC_FOLDER, record.downloadKey()).exists()) continue;
-            DATA_STORAGE.removeSchematic(record);
-            logger.info("Removed schematic {} because no file system entry was present", record.downloadKey());
-            deletionCounter++;
-        }
-        return deletionCounter;
-    }
-
-    private static int danglingPrune() {
-        var deletionCounter = 0;
-        var files = SCHEMATIC_FOLDER.listFiles();
-        if (files != null) for (var file : files) {
-            try {
-                DATA_STORAGE.getSchematicByDownloadKey(file.getName());
-            } catch (SQLException e) {
-                logger.debug("Deleting dangling file {}", file.getName());
-                var success = file.delete();
-                if (!success) logger.error("Failed to delete dangling file {}", file.getName());
-                else deletionCounter++;
-            }
-        }
-        return deletionCounter;
-    }
-
-    private static int outdatedPrune() throws SQLException {
-        var deleted = DATA_STORAGE.removeSchematics();
-        for (var record : deleted) {
-            var success = new File(SCHEMATIC_FOLDER, record.downloadKey()).delete();
-            if (!success) logger.error("Failed to delete file: {}", record.downloadKey());
-        }
-        return deleted.size();
+    private void setupAccessControl() {
+        javalin.before(context -> context.header("Access-Control-Allow-Origin", config.allowedOrigin()));
     }
 }
