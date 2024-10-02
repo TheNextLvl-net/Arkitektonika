1fdf603dd6be43a6891f0260ffb2ce758fe78621
david <mrminecraft00@gmail.com>
2024-10-02 18:11:06 +0200

    Add VersionChecker for handling versioning via GitHub releases
    Introduced `VersionChecker` class to manage version checks for the Arkitektonika project using GitHub releases. The new implementation logs whether the running version is the latest, needs an update, or is a snapshot.

---
+++ src/main/java/net/thenextlvl/arkitektonika/version/VersionChecker.java
@@ -0,0 +1,46 @@
+package net.thenextlvl.arkitektonika.version;
+
+import core.version.SemanticVersion;
+import core.version.github.GitHubVersionChecker;
+import core.version.github.Release;
+import lombok.Getter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Getter
+public class VersionChecker extends GitHubVersionChecker<SemanticVersion> {
+    private static final Logger logger = LoggerFactory.getLogger(VersionChecker.class);
+
+    private final SemanticVersion versionRunning = parseVersion(
+            getClass().getPackage().getImplementationVersion()
+    );
+
+    public VersionChecker() {
+        super("TheNextLvl", "Arkitektonika");
+    }
+
+    @Override
+    public SemanticVersion parseVersion(String version) {
+        return SemanticVersion.parse(version);
+    }
+
+    @Override
+    public boolean isSupported(Release release) {
+        return true;
+    }
+
+    public void checkVersion() {
+        retrieveLatestSupportedVersion().thenAccept(version -> {
+            if (version.equals(versionRunning)) {
+                logger.info("You are running the latest version of Arkitektonika");
+            } else if (version.compareTo(versionRunning) > 0) {
+                logger.warn("An update for Arkitektonika is available");
+                logger.warn("You are running version {}, the latest supported version is {}", versionRunning, version);
+                logger.warn("Update at https://github.com/{}/{}", getOwner(), getRepository());
+            } else logger.warn("You are running a snapshot version of Arkitektonika");
+        }).exceptionally(throwable -> {
+            logger.error("Version check failed", throwable);
+            return null;
+        });
+    }
+}
